ff59a889f302791b2c4d8c10f63c10324bfaa4ce
fader-azevedo <fadermacuvele383@gmail.com>
2018-03-23 19:44:46 +0200

    tabela primeira ordem

--- src/RungeKuttaRk4.java
+++ src/RungeKuttaRk4.java
@@ -18,7 +18,7 @@ public class RungeKuttaRk4 extends JFrame {
 
     private final DefaultTableModel model = new DefaultTableModel();
     private String orderm;
-    private String[] colunasOrdem1 = {"i", "Xi", "Yi", "F", "Yi+1"};
+    private final String[] colunasOrdem1 = {"i", "Xi", "Yi", "F", "Yi+1"};
     private String[] colunasOrdem2 = {"i", "Xi", "Yi", "F", "Yi+1"};
     private String[] colunasOrdem3 = {"i", "Xi", "Yi", "K1", "K2", "K3", "Yi+1"};
     private String[] colunasOrdem4 = {"i", "Xi", "Yi", "K1", "K2", "K3", "K4", "Yi+1"};
